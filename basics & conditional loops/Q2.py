172e154d8fafc39d4bc5e8d3f5246f48df709dc0
Kiran Kumar <88982113+kirankumar-7@users.noreply.github.com>
2021-09-22 21:56:17 +0530

    Add files via upload

---
+++ basics & conditional loops/Q2.py
@@ -0,0 +1,3 @@
+fname=input("enter the first name ")
+lname=input("enter the lname name ")
+print(fname[::-1]+" "+lname[::-1])
---
+++ basics & conditional loops/Q3.py
@@ -0,0 +1,4 @@
+d=12
+r=d//2
+v=(4/3 *3.14* r**3)
+print(f"The volume of the sphere is {v}")
